b74d9a2180790b81f552a77e6d04f5dc740e73ef
WindChang1999 <47917003+WindChang1999@users.noreply.github.com>
2019-05-02 21:16:21 +0800

    更新一些架构
    已完成功能分块
    取消分时
    未完成：
    定位具体的实现
    转弯具体的实现

--- car/car.ino
+++ car/car.ino
@@ -1,19 +1,16 @@
 #include <FlexiTimer2.h>
 
+typedef struct point{
+  int x;
+  int y;
+} Point;
 
-int Direction = 0;
-// 标志当前车头朝向
-// Direction = 0 -> y轴正向  右
-// Direction = 1 -> x轴负向  上
-// Direction = 2 -> y轴负向  左
-// Direction = 3 -> x轴正向  下
-// 左转时 Direction = ( Direction+1 ) % 4
-// 右转时 Direction = ( Direction-1 ) % 4
-// if Direction < 0{ Direction += 4 }
+typedef struct quene{     // 用于储存路径的队列
+  int front;
+  int rear;
+  Point Road[80];
+} Quene;
 
-
-int Position_X = 0;
-int Position_Y = 0;
 const bool Map[12][12] = {{0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,1,1,1,1,1,1,1,1,1,1,0},
                           {0,1,1,1,1,1,1,1,1,1,1,0},
@@ -26,6 +23,8 @@ const bool Map[12][12] = {{0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,1,1,1,1,1,1,1,1,1,1,0},
                           {0,1,1,1,1,1,1,1,1,1,1,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0}};
+
+
 
 
 /* 引脚定义 */
@@ -41,7 +40,7 @@ int IN3 = 36;
 int IN4 = 38; //引脚改动
 int PWMA = 7; //左轮
 int PWMB = 8;
-int time_2,time_1;
+
 int TrigPin = 8;
 int EchoPin = 9;
 //下为红外部分引脚
@@ -73,7 +72,14 @@ int setpoint_R = 20;
 #define Value_Setpoint 20
 int Value_Red_ForWard[4] = {0};
 int Value_Red_Center[4] = {0};
+int Direction = 0;  //车头方向
 
+
+//  5-1 运动模态
+int M;  // 底层运动=1, 上层运动=2, 拍摄=3
+int move_state;   //
+int  turn_direction;
+bool turn_complete;     // 转弯完成标志, 在给出转弯信号的同时置false即可
 
 //  pid
 double PulseCounter_L = 0, PulseCounter_R = 0;    //左右电机编码器脉冲计数器
@@ -84,29 +90,10 @@ static int error_last2, error_int2, derror2;
 int count2 = 0;
 int count_Ti = 5;
 
-
-
-
-//  5-1 
-int M;  // 底层运动=1, 上层运动=2, 拍摄=3
-int move_state;   //
-int  turn_direction;
-bool turn_complete;     // 转弯完成标志, 在给出转弯信号的同时置false即可
-
-
-typedef struct point{
-  int x;
-  int y;
-} Point;
-
-typedef struct quene{     // 用于储存路径的队列
-  int front;
-  int rear;
-  Point Road[80];
-} Quene;
-
+// 位置坐标管理
 Quene Q;
 Point Target_Point, tem_Point;      // 目标点和临时点
+Point Next_Point, Now_Point;
 
 
 void Enquene( Point P ){
@@ -123,6 +110,17 @@ bool QueneIsEmpty(){
 }
 
 
+void Start_PID(void){
+  PulseCounter_L = 0, PulseCounter_R = 0;
+  error_last1 = error_int1 = derror1 = 0;
+  error_last2 = error_int2 = derror2 = 0;
+  FlexiTimer2::set(10, calculate_pid);   // 定时中断计算pid
+  FlexiTimer2::start();
+}
+
+void Stop_PID(void){
+  FlexiTimer2::stop();
+}
 
 void calculate_pid(){
     double Kp = 1.45, Ki = 1/count_Ti*Kp, Kd = 5*Kp;
@@ -212,7 +210,7 @@ double pid2(int Pulse, float Kp, float Ki, float Kd){
 
 
 /**************************************************************************
-函数功能：赋值给PWM寄存器 作者：平衡小车之家
+函数功能：这里根据V_NOW_L和V_NOW_R的正负决定电机的转动方向, 并且给PWMA、PWMB赋值
 入口参数：MotorA, MotorB
 返回  值：无
 **************************************************************************/
@@ -225,8 +223,9 @@ void Set_Pwm()
   else        digitalWrite(IN3, LOW),      digitalWrite(IN4, HIGH); //TB6612的电平控制
   analogWrite(PWMB, abs(V_NOW_R));//赋值给PWM寄存器
 }
+
 /**************************************************************************
-函数功能：限制PWM赋值  作者：平衡小车之家
+函数功能：限制PWM赋值  
 入口参数：无
 返回  值：无
 **************************************************************************/
@@ -241,8 +240,10 @@ void Limit_Pwm(void)
   if (V_NOW_L < -Amplitude) V_NOW_L = -Amplitude;
   if (V_NOW_L > Amplitude)  V_NOW_L = Amplitude;
 }
+
+
 /**************************************************************************
-函数功能：控制小车转向  作者：Ding
+函数功能：控制小车的运动状态，直行、倒车、左右转等, 并赋值给V_NOW_L和V_NOW_R
 入口参数：Receive_Data
 返回  值：无
 **************************************************************************/
@@ -260,8 +261,6 @@ void Control(int Receive_Data){
         V_NOW_R = NormalSpeed;
         setpoint_L = Value_Setpoint;
         setpoint_R = Value_Setpoint-1;
-        Limit_Pwm();
-        Set_Pwm();
         Serial.println("直行");
     }
     if(Flag_Backward) {
@@ -269,16 +268,12 @@ void Control(int Receive_Data){
         V_NOW_R = -NormalSpeed;
         setpoint_L = -Value_Setpoint+1;
         setpoint_R = -Value_Setpoint;
-        Limit_Pwm();
-        Set_Pwm();
     }
     if(Flag_Left) {
         V_NOW_L = -NormalSpeed ;
         V_NOW_R = NormalSpeed ;
         setpoint_L = -Value_Setpoint+1;
         setpoint_R = Value_Setpoint;
-        Limit_Pwm();
-        Set_Pwm();
         Serial.println("左转");
     }
     if(Flag_Right) {
@@ -286,10 +281,10 @@ void Control(int Receive_Data){
         V_NOW_R = -NormalSpeed ;
         setpoint_L = Value_Setpoint-1;
         setpoint_R = -Value_Setpoint;
-        Limit_Pwm();
-        Set_Pwm();
         Serial.println("右转");
     }
+    Set_Pwm();
+    Limit_Pwm();
 }
 
 
@@ -352,15 +347,11 @@ void Rectify(){
 
     if(Value_Red_ForWard[0] && Value_Red_ForWard[1] &&//左大出界，向右纠偏
             !Value_Red_ForWard[2] && !Value_Red_ForWard[3]){
-                setpoint_R = Value_Setpoint - 4;
-                Limit_Pwm();
-                Set_Pwm();
+                setpoint_R = Value_Setpoint - 2;
     }
     else if(!Value_Red_ForWard[0] && !Value_Red_ForWard[1] &&//右大出界，向左纠偏
             Value_Red_ForWard[2] && Value_Red_ForWard[3]){
-                setpoint_L = Value_Setpoint - 4;
-                Limit_Pwm();
-                Set_Pwm();
+                setpoint_L = Value_Setpoint - 2;
     }
 //    else if(Value_Red_ForWard[0] && Value_Red_ForWard[1] && //停止
 //            Value_Red_ForWard[2] && Value_Red_ForWard[3]){
@@ -369,34 +360,26 @@ void Rectify(){
     else if(Value_Red_ForWard[0] && !Value_Red_ForWard[1] && //正常
             !Value_Red_ForWard[2] && Value_Red_ForWard[3]){
                 setpoint_L = setpoint_R = Value_Setpoint;
-                Limit_Pwm();
-                Set_Pwm();
     }
     else if(Value_Red_ForWard[0] && Value_Red_ForWard[1] && //左出界
             !Value_Red_ForWard[2] && Value_Red_ForWard[3]){
-                 setpoint_R = Value_Setpoint - 2;
-                 Limit_Pwm();
-                 Set_Pwm();
+                 setpoint_R = Value_Setpoint - 1;
     }
     else if(Value_Red_ForWard[0] && !Value_Red_ForWard[1] && //右出界
             Value_Red_ForWard[2] && Value_Red_ForWard[3]){
-                 setpoint_L = Value_Setpoint - 2;
-                 Limit_Pwm();
-                 Set_Pwm();
+                 setpoint_L = Value_Setpoint - 1;
     }
     else if(Value_Red_ForWard[0] && Value_Red_ForWard[1] && //左出界
             Value_Red_ForWard[2] && !Value_Red_ForWard[3]){
-                 setpoint_R = Value_Setpoint - 5;
-                 Limit_Pwm();
-                 Set_Pwm();
+                 setpoint_R = Value_Setpoint - 3;
     }
     else if(!Value_Red_ForWard[0] && Value_Red_ForWard[1] && //右出界
             Value_Red_ForWard[2] && Value_Red_ForWard[3]){
-                 setpoint_L = Value_Setpoint - 5;
+                 setpoint_L = Value_Setpoint - 3;
                  //V_NOW_R = NormalSpeed;
-                 Limit_Pwm();
-                 Set_Pwm();
     }
+    Limit_Pwm();
+    Set_Pwm();
  }
 
 
@@ -405,79 +388,32 @@ void Rectify(){
 入口参数：无
 返回  值：无
 **************************************************************************/
-void Rectify_Turn(){
-    if(!Value_Red_Center[0] && Value_Red_Center[1] &&
-       !Value_Red_Center[2] && Value_Red_Center[3]){
-        Flag_Count = 0;
-//        Serial.println("1");
-//        Serial.println(Position_X);
-       }
-    if(!Value_Red_Center[0] && !Value_Red_Center[1] &&
-       !Value_Red_Center[2] && !Value_Red_Center[3]){
-            if(Flag_Count == 0){
-              Position_X ++;
-              Flag_Count = 1;
-            }
-            if(Position_X == 4){
-              Turn_Left();
-              Control(1);
-            }
-            if(Position_X == 5){
-              Turn_Left();
-              Control(1);
-              Position_X = 0;
-            }
-    }
- }
+// void Rectify_Turn(){
+//     if(!Value_Red_Center[0] && Value_Red_Center[1] &&
+//        !Value_Red_Center[2] && Value_Red_Center[3]){
+//         Flag_Count = 0;
+// //        Serial.println("1");
+// //        Serial.println(Position_X);
+//        }
+//     if(!Value_Red_Center[0] && !Value_Red_Center[1] &&
+//        !Value_Red_Center[2] && !Value_Red_Center[3]){
+//             if(Flag_Count == 0){
+//               Position_X ++;
+//               Flag_Count = 1;
+//             }
+//             if(Position_X == 4){
+//               Turn_Left();
+//               Control(1);
+//             }
+//             if(Position_X == 5){
+//               Turn_Left();
+//               Control(1);
+//               Position_X = 0;
+//             }
+//     }
+//  }
 
  
- /**************************************************************************
-函数功能：读取红外传感当前电平  作者：Ding
-入口参数：无
-返回  值：无
-**************************************************************************/
-void Read_RedValue(){
-    Value_Red_ForWard[0] = digitalRead(Red_Forward_0);
-    Value_Red_ForWard[1] = digitalRead(Red_Forward_1);
-    Value_Red_ForWard[2] = digitalRead(Red_Forward_2);
-    Value_Red_ForWard[3] = digitalRead(Red_Forward_3);
-    Value_Red_Center[0] = digitalRead(Red_Center_0);
-    Value_Red_Center[1] = digitalRead(Red_Center_1);
-    Value_Red_Center[2] = digitalRead(Red_Center_2);
-    Value_Red_Center[3] = digitalRead(Red_Center_3);
-}
-/**************************************************************************
-函数功能：碰撞开关  作者：Ding
-入口参数：
-返回  值：无
-**************************************************************************/
-void Collide(){
-
-}
-/**************************************************************************
-函数功能：超声波传感  作者：Ding
-入口参数：
-返回  值：无
-**************************************************************************/
-void US_100(){
-    digitalWrite(TrigPin,LOW);
-    delayMicroseconds(2);
-    digitalWrite(TrigPin,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TrigPin,LOW);
-
-    cm = pulseIn(EchoPin,HIGH)/58.8 ;
-    cm = (int(cm*100.0)) / 100.0;
-    Serial.print(cm);
-    Serial.print("cm");
-    Serial.println();
-    delay(1000);
-}
-
-
-
-
-
 
 void setup() {
     // put your setup code here, to run once:
@@ -512,61 +448,38 @@ void setup() {
 
     Serial.begin(9600);
     attachInterrupt(0,Stop,LOW); //外部中断，停止
-    FlexiTimer2::set(5, Main_Loop);   // 状态机控制
-    FlexiTimer2::start();
-
     attachInterrupt(3,ReadEncoder_L, CHANGE);
     attachInterrupt(2,ReadEncoder_R, CHANGE);
+
+    Now_Point.x = Now_Point.y = 0;
 
 }
 
 
-
-
 void Movement_block(void) {
-    // 读红外
-  Read_RedValue();
-    // pid 计算
+  //定位函数, 给出当前坐标值
+  // 先计算下个状态的direction
+  // Cal_Direction();
+  // pid 计算
   calculate_pid();
   switch (move_state)
   {
     case 1:
-      //保持巡线
-      break;
     case 2:
-      Movement_block_turn();
-      break;
     case 3:
+    case 4:
+      Control(move_state);
+      break;
+    case 5:
       //停车
+      Stop();
       break;
     default:
       Serial.println("Movement_block, case异常情况");
       break;
   }
-
 }
 
-void Movement_block_turn(void){
-  switch (turn_direction)
-  {
-    case 1:
-      // 右转
-      break;
-    case 2:
-      // 左转
-    default:
-      break;
-  }
-
-  if (// 未到位) {
-    turn_complete = false;
-    turn_direction = turn_direction;
-  }
-  if (// 到位) {
-    turn_complete = true;
-  }
-  
-}
 
 void Servo_Stepper_block(void){
 
@@ -576,94 +489,56 @@ void Classfy_block(void){
 
 }
 
-void Main_Loop(void) {
-  switch (M)
-  {
-    case 1:
-      Movement_block();
-      break;
-    case 2:
-      Servo_Stepper_block();
-    case 3:
-      Classfy_block();
-    default:
-      break;
-  }
-}
 
 void loop{
-  // 空循环, 等定时器溢出
-}
-
-
-
-
-/**
- * 读取右轮编码器脉冲数量,修改PulseCounter_R
-**/
-/**
- * 读取左轮编码器脉冲数量,修改PulseCounter_L
-**/
-void ReadEncoder_L(void){
-  /* A相检测到上升沿时，B为LOW为正转，B为HIGH为反转
-     A相检测到下降沿时，B为HIGH为正转，B为LOW为反转 */
-  if (digitalRead(ENCODER_L_A) == HIGH) {
-    //A相检测到上升沿
-    if (digitalRead(ENCODER_L_B) == LOW) {
-      //正转
-      PulseCounter_L--;
+    if(digitalRead(collide_1) == LOW) {
+        Flag_Begin = 1;
     }
-    else{
-      //反转
-      PulseCounter_L++;
+    if(Flag_Begin == true && move_state == 0){
+        M = 1;
+        move_state = 1;
     }
-  }
-
-  else{
-    //A相检测到下降沿
-    if(digitalRead(ENCODER_L_B) == LOW){
-      //反转
-      PulseCounter_L++;
-    }
-    else{
-      //正转
-      PulseCounter_L--;
-    }
+    if (Flag_Begin == 1) {
+      switch (M){
+        case 1:
+          Movement_block();
+          break;
+        case 2:
+          Servo_Stepper_block();
+        case 3:
+          Classfy_block();
+        default:
+          break;
+      }
   }
 }
 
-/**
- * 读取右轮编码器脉冲数量,修改PulseCounter_R
-**/
-void ReadEncoder_R(void){
-  /* A相检测到上升沿时，B为LOW为正转，B为HIGH为反转
-     A相检测到下降沿时，B为HIGH为正转，B为LOW为反转 */
-  if (digitalRead(ENCODER_R_A) == HIGH) {
-    //A相检测到上升沿
-    if (digitalRead(ENCODER_R_B) == LOW) {
-      //正转
-      PulseCounter_R++;
+
+
+/* ---------------------------------------------------------------------- */
+/* 定位程序 */
+/* ---------------------------------------------------------------------- */
+void Location(void){
+  // 读所有红外传感器
+  Read_RedValue();
+  // 如果检测到定位红外的下降沿, 那么要对Now_Point进行修改
+  if (/*检测到定位红外的下降沿*/) {
+    Now_Point = Next_Point;
+    if (QueneIsEmpty() == false) {
+      Next_Point = Dequene();
     }
     else{
-      //反转
-      PulseCounter_R--;
+      // 路径结束, 转其他block操作
     }
   }
-
-  else{
-    //A相检测到下降沿
-    if(digitalRead(ENCODER_R_B) == LOW){
-      //反转
-      PulseCounter_R--;
-    }
-    else{
-      //正转
-      PulseCounter_R++;
-    }
+  else{ // 没有检测到下降沿，还未触线，保持状态不变。
   }
 }
 
-//////3.30 20.17增加定位
+
+/* ---------------------------------------------------------------------- */
+/* 障碍物检查程序，检查一条直线上是否存在障碍物 */
+/* ---------------------------------------------------------------------- */
 
 bool Check_obstacle_x(int x0,int y1,int y2){  // 检查从(x0, y1)到(x0, y2)的水平运动直线上是否有障碍物
   // 遍历Map中的x0行即可
@@ -701,7 +576,42 @@ bool Check_obstacle_y(int y0,int x1,int x2){  // 检查从(x1, y0)到(x2, y0)的
   return HaveObastacle;
 }
 
-void Adjust_Direction(int Target){
+
+/* ---------------------------------------------------------------------- */
+/* 方向计算程序：根据当前坐标和下一个临接点的坐标计算得到应走的方向 */
+/* 输入: Now.Point, Next_Point */
+/* 影响: M, move_state */
+/* ---------------------------------------------------------------------- */
+
+// 标志当前车头朝向
+// Direction = 0 -> y轴正向  右
+// Direction = 1 -> x轴负向  上
+// Direction = 2 -> y轴负向  左
+// Direction = 3 -> x轴正向  下
+// 左转时 Direction = ( Direction+1 ) % 4
+
+// 右转时 Direction = ( Direction-1 ) % 4
+// if Direction < 0{ Direction += 4 }
+
+
+void Cal_Direction(void){
+/* 先从Next_Point和当前坐标之间的关系得到Target_Direction */
+  if (Now_Point.x == Next_Point.x - 1 && Now_Point.y == Next_Point.y) {
+    Target = 3;
+  }
+  else if (Now_Point.x == Next_Point.x + 1 && Now_Point.y == Next_Point.y) {
+    Target = 1;
+  }
+  else if (Now_Point.x == Next_Point.x && Now_Point.y == Next_Point.y - 1) {
+    Target = 0;
+  }
+  else if (Now_Point.x == Next_Point.x && Now_Point.y == Next_Point.y + 1) {
+    Target = 2;
+  }
+  else {
+    Serial.println("Error: Cal_Direction中Now_Point和Next_Point的关系错误");
+    return;
+  }
 //   Serial.print("当前位置 ");
 //   Serial.print("(X ,Y) == (");
 //   Serial.print(Position_X);
@@ -713,26 +623,41 @@ void Adjust_Direction(int Target){
 //   Serial.print("    Target: ");
 //   Serial.println(Target);
   if (Target == Direction) {
-    return;
+    // 保持巡线
+    M = 1;
+    move_state = 1;
   }
   if (Target - Direction == 2 || Target - Direction == -2) {
     // Serial.println("转180度");
     //转180度
+    M = 1;
+    move_state = 4;
   }
   else if (Target - Direction == 1 || Target - Direction == -3) {
     // Serial.println("左转90度");
     //左转90度
+    M = 1;
+    move_state = 2;
   }
   else if (Target - Direction == -1 || Target - Direction == 3) {
     // Serial.println("右转90度");
     //右转90度
+    M = 1;
+    move_state = 3;
   }
   else{
-    // Serial.println("Adjust_Direction,调整方向异常");
+     Serial.println("Adjust_Direction,调整方向异常");
   }
 }
 
-void Move_Horizontal(int x0, int y1, int y2){ // 水平移动(x0, y1) -->(x0, y2)
+
+/* ------------------------------------------------------------------------------------ */
+/* 路径规划程序：计算路径并放入队列Q中 */
+/* 输入：起始点坐标(x1, y1)， 终点坐标(x2, y2) */
+/* 影响：路径队列Q */
+/* ------------------------------------------------------------------------------------ */
+
+void Path_Planning_Horizontal(int x0, int y1, int y2){ // 水平移动(x0, y1) -->(x0, y2)
 //   Serial.print("水平移动: ");
 //   Serial.print("(");
 //   Serial.print(x0);
@@ -771,24 +696,24 @@ void Move_Horizontal(int x0, int y1, int y2){ // 水平移动(x0, y1) -->(x0, y2
   else{ // 如果有障碍物
     if(x0 <= 5){
       // 移动到x = 3, 即(x0, y1) --> (3, y1)
-      Move_Vertical(y1, x0, 3);
+      Path_Planning_Vertical(y1, x0, 3);
       // 然后再从 (3, y1) --> (3, y2)
-      Move_Horizontal(3, y1, y2);
+      Path_Planning_Horizontal(3, y1, y2);
       // (3, y2) --> (x0, y2)
-      Move_Vertical(y2, 3, x0);
+      Path_Planning_Vertical(y2, 3, x0);
     }
     else{
       // (x0, y1) --> (8, y1)
-      Move_Vertical(y1, x0, 8);
+      Path_Planning_Vertical(y1, x0, 8);
       // (8, y1) --> (8, y2)
-      Move_Horizontal(8, y1, y2);
+      Path_Planning_Horizontal(8, y1, y2);
       // (8, y2) --> (x0, y2)
-      Move_Vertical(y2, 8, x0);
+      Path_Planning_Vertical(y2, 8, x0);
     }
   }
 }
 
-void Move_Vertical(int y0, int x1, int x2){ // 垂直移动(x1, y0) -->(x2, y0)
+void Path_Planning_Vertical(int y0, int x1, int x2){ // 垂直移动(x1, y0) -->(x2, y0)
 //   Serial.print("垂直移动: ");
 //   Serial.print("(");
 //   Serial.print(x1);
@@ -826,29 +751,85 @@ void Move_Vertical(int y0, int x1, int x2){ // 垂直移动(x1, y0) -->(x2, y0)
 //   Serial.println("存在障碍");  
     if(y0 <= 5){
     //   // 移动到y = 3, 即(x1, y0) --> (x1, 3)
-      Move_Horizontal(x1, y0, 3);
+      Path_Planning_Horizontal(x1, y0, 3);
     //   // 然后再从 (x1, 3) --> (x2, 3)
-      Move_Vertical(3, x1, x2);
+      Path_Planning_Vertical(3, x1, x2);
     //   // (x2, 3) --> (x2, y0)
-      Move_Horizontal(x2, 3, y0);
+      Path_Planning_Horizontal(x2, 3, y0);
     }
     else{
     //   // (x1, y0) --> (x1, 8)
-      Move_Horizontal(x1, y0, 8);
+      Path_Planning_Horizontal(x1, y0, 8);
     //   // (x1, 8) --> (x2, 8)
-      Move_Vertical(8, x1, x2);
+      Path_Planning_Vertical(8, x1, x2);
     //   // (x2, 8) --> (x2, y0)
-      Move_Horizontal(x2, 8, y0);
+      Path_Planning_Horizontal(x2, 8, y0);
     }
   }
 }
 
-void MOVE(int x1, int y1, int x2, int y2){
+void Path_Planning(int x1, int y1, int x2, int y2){
   Q.rear = -1;
   Q.front = 0;
-  Move_Horizontal(x1, y1, y2);
-  Move_Vertical(y2, x1, x2);
+  Path_Planning_Horizontal(x1, y1, y2);
+  Path_Planning_Vertical(y2, x1, x2);
 }
 
+/* ---------------------------------------------------------------------- */
+/* 左右轮电机脉冲数读取 */
+/* ---------------------------------------------------------------------- */
+void ReadEncoder_L(void){
+  /* A相检测到上升沿时，B为LOW为正转，B为HIGH为反转
+     A相检测到下降沿时，B为HIGH为正转，B为LOW为反转 */
+  if (digitalRead(ENCODER_L_A) == HIGH) {
+    //A相检测到上升沿
+    if (digitalRead(ENCODER_L_B) == LOW) {
+      //正转
+      PulseCounter_L--;
+    }
+    else{
+      //反转
+      PulseCounter_L++;
+    }
+  }
 
+  else{
+    //A相检测到下降沿
+    if(digitalRead(ENCODER_L_B) == LOW){
+      //反转
+      PulseCounter_L++;
+    }
+    else{
+      //正转
+      PulseCounter_L--;
+    }
+  }
+}
 
+void ReadEncoder_R(void){
+  /* A相检测到上升沿时，B为LOW为正转，B为HIGH为反转
+     A相检测到下降沿时，B为HIGH为正转，B为LOW为反转 */
+  if (digitalRead(ENCODER_R_A) == HIGH) {
+    //A相检测到上升沿
+    if (digitalRead(ENCODER_R_B) == LOW) {
+      //正转
+      PulseCounter_R++;
+    }
+    else{
+      //反转
+      PulseCounter_R--;
+    }
+  }
+
+  else{
+    //A相检测到下降沿
+    if(digitalRead(ENCODER_R_B) == LOW){
+      //反转
+      PulseCounter_R--;
+    }
+    else{
+      //正转
+      PulseCounter_R++;
+    }
+  }
+}
